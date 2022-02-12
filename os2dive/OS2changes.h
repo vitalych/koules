a5e0613ca6d4cd4a2e51602c253c1fd99de2df74
Vitaly Chipounov <vitaly@chipounov.fr>
2022-02-12 22:22:53 +0100

    Removed unused files
    Signed-off-by: Vitaly Chipounov <vitaly@chipounov.fr>

--- os2dive/OS2changes.h
+++
@@ -1,3 +0,0 @@
-/* Changes for OS/2 Warp with Dive.                        *
- *  Copyright(c)1996 by Thomas A. K. Kjaer                  *
- ***********************************************************/
